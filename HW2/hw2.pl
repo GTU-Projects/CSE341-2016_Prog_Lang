e550797ca95311f5132b7fc74015da115b51c5c5
hmenn <hmen.56@gmail.com>
2016-12-12 03:02:35 +0300

    [hw2] part3,part4
    part3 completed, part4.2 completed

--- HW2/hw2.pl
+++ HW2/hw2.pl
@@ -1,7 +1,7 @@
 % Prolog HW2 - HASAN MEN 131044009
-% -------
-%  PART1
-% -------
+%---------------%
+%---  PART 1 ---%
+%---------------%
 
 %facts : flight routes
 flight(istanbul,izmir).
@@ -41,7 +41,11 @@ flight(kars,istanbul).
 flight(trabzon,istanbul).
 flight(trabzon,ankara).
 
-%Part2 distances facts
+%---------------%
+%---  PART 2 ---%
+%---------------%
+
+%distances facts
 distance(istanbul,izmir,328).
 distance(istanbul,antalya,482).
 distance(istanbul,konya,461).
@@ -79,18 +83,143 @@ distance(kars,istanbul,1189).
 distance(trabzon,istanbul,902).
 distance(trabzon,ankara,593).
 
+
+
 % a predicate indicating there exist a route between X and Y if there is
 % flight between X and Y
-
-member(X,[X|_]).
-member(X,[_|Tail]):- member(X,Tail).
-
-getEmptyList([]).
-getListCopy(L,L).
 
 addList([],L,L).
 addList(Element,L,[Element|L]).
 
+getEmptyList([]).
+getListCopy(L,L).
+
+member(X,[X|_]).
+member(X,[_|Tail]):- member(X,Tail).
+
+
 getNextCity(X,Y,Old):-
   flight(X,Y),
   not(member(Y,Old)).
+
+
+path([B | Rest], B, [B | Rest], Length, Length).
+path([A | Rest], B, Path, CurrentLength, Length) :-
+    distance(A, C, X),
+    \+member(C, [A | Rest]),
+    NewLength is CurrentLength + X,
+    path([C, A | Rest], B, Path, NewLength, Length).
+
+find_paths(A, B) :-
+    path([A], B, Path, 0, Length),
+    printPath(Path),
+    writef(' with evaluation %d\n', [Length]),
+    fail.
+
+path1(A,B,Q) :-
+       travel(A,B,[A],Q).
+
+travel(A,B,P,[B|P]) :-
+       flight(A,B).
+travel(A,B,Visited,Path) :-
+       flight(A,C),
+       C \== B,
+       \+member(C,Visited),
+       travel(C,B,[C|Visited],Path).
+
+
+
+%---------------%
+%---  PART 3 ---%
+%---------------%
+% completed
+when(102,10).
+when(108,12).
+when(341,14).
+when(455,16).
+when(452,17).
+
+where(102,z23).
+where(108,z11).
+where(341,z06).
+where(455,z07).
+where(452,z07).
+
+enrol(a,102).
+enrol(a,108).
+enrol(b,102).
+enrol(c,108).
+enrol(d,341).
+enrol(e,455).
+
+% 3.1
+schedule(X,P,T):- enrol(X,P), when(P,T).
+
+% 3.2
+usage(R,T):- where(T,R).
+
+% 3.3
+confClass(X,Y):- where(X,A),
+                where(Y,B),
+                A==B.
+
+confTime(X,Y):-  when(X,A),
+                 when(Y,B),
+                 A == B.
+conflict(X,Y):- not((not(confClass(X,Y)),not(confTime(X,Y)))).
+
+
+% 3.4
+meet(X,Y):- enrol(X,C1),where(C1,P1),
+            enrol(Y,C2),where(C2,P2),
+            C1==C2,P1==P2,!.
+
+
+%---------------%
+%---  PART 4 ---%
+%---------------%
+
+% 4.1
+
+
+% 4.2
+uniqueHelper([],L,L).
+
+uniqueHelper([H|T],L1,L2):-
+  member(H,L1),
+  uniqueHelper(T,L1,L2).
+
+uniqueHelper([H|T],L1,L2):-
+  uniqueHelper(T,[H|L1],L2).
+
+unique(List,Set):-
+  uniqueHelper(List,[],Set),!.
+
+% 4.3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%
