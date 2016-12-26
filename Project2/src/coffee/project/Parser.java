de13db5ff01fa252b89bf0d43d9c2968f5d61a80
hmenn <hmen.56@gmail.com>
2016-12-26 15:34:53 +0300

    [project2] stack doldur bosalt ve id icin reduce

--- Project2/src/coffee/project/Parser.java
+++ Project2/src/coffee/project/Parser.java
@@ -5,6 +5,7 @@ import coffee.TokenList;
 import coffee.datatypes.*;
 
 import java.util.Collections;
+import java.util.EmptyStackException;
 import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,38 +16,43 @@ import java.util.regex.Pattern;
 public class Parser {
     // Parses the lexer result and prints *ONLY* the parsing result.
 
-    private final String EXPI_EXPRESSION="(\\((\\+|\\-|\\*|\\/) EXPI EXPI\\))|(Id)|(IntegerValue)|(\\(Id EXPLISTI\\))";
-    private final String EXPB_EXPRESSION="(\\((and|or|equal) EXPB EXPB\\)|\\(equal EXPI EXPI\\)|BinaryValue|\\(not EXPB\\))";
-    private final String EXPLISTI="(\\(concat EXPLISTI EXPLISTI\\)|\\(append EXPI EXPLISTI\\)|LISTVALUE|null)";
-    private final String EXPI_ASSINGMENT="(\\(set Id EXPI\\))";
-    private final String EXPI_FUNC="(\\(deffun Id IDLIST EXPLISTI\\))";
-    private final String EXPI_IDLIST="empty"; // TODO: EMPTY
-    private final String EXPI_FUNC_CALL="(\\(Id EXPLISTI\\))";
-    private final String EXPI_IF="(\\(if EXPB (then EXPLISTI else EXPLISTI|EXPLISTI|EXPLISTI EXPLISTI)\\))";
-    private final String EXPB_WHILE="(\\(while \\(EXPB\\) EXPLISTI\\))";
-    private final String EXPB_FOR="(\\(for \\(Id EXPI EXPI\\) EXPLISTI\\))";
+    private final String EXPI_EXPRESSION = "(\\((\\+|\\-|\\*|\\/) EXPI EXPI\\))|(Id)|(IntegerValue)|(\\(Id EXPLISTI\\))";
+    private final String EXPB_EXPRESSION = "(\\((and|or|equal) EXPB EXPB\\)|\\(equal EXPI EXPI\\)|BinaryValue|\\(not EXPB\\))";
+    private final String EXPLISTI = "(\\(concat EXPLISTI EXPLISTI\\)|\\(append EXPI EXPLISTI\\)|LISTVALUE|null)";
+    private final String EXPI_ASSINGMENT = "(\\(set Id EXPI\\))";
+    private final String EXPI_FUNC = "(\\(deffun Id IDLIST EXPLISTI\\))";
+    private final String EXPI_IDLIST = "empty"; // TODO: EMPTY
+    private final String EXPI_FUNC_CALL = "(\\(Id EXPLISTI\\))";
+    private final String EXPI_IF = "(\\(if EXPB (then EXPLISTI else EXPLISTI|EXPLISTI|EXPLISTI EXPLISTI)\\))";
+    private final String EXPB_WHILE = "(\\(while \\(EXPB\\) EXPLISTI\\))";
+    private final String EXPB_FOR = "(\\(for \\(Id EXPI EXPI\\) EXPLISTI\\))";
     private Matcher matcher;
     private Stack<String> reducedStack = new Stack<>();
     private Stack<Token> tokenStack = new Stack<>();
 
+    IdentifierList identifierList = IdentifierList.getInstance();
+    TokenList tokenList = TokenList.getInstance();
 
-    public Parser(){}// TODO: belki patternler buraya gelebilir
+    public Parser() {
+    }// TODO: belki patternler buraya gelebilir
 
     public void parse() {
-        IdentifierList identifierList = IdentifierList.getInstance();
-        TokenList tokenList = TokenList.getInstance();
 
 
         tokenStack.addAll(tokenList.getAllTokens());
         Collections.reverse(tokenStack);
 
-        System.out.println("Token Stack:"+tokenStack);
-        while(!tokenStack.empty()){
+        System.out.println("Token List:" + tokenList.getAllTokens());
+        System.out.println("Token Stack:" + tokenStack);
+        int i = 0;
+        while (!tokenStack.empty()) {
             // eger stack reduce edilebilir ise reduce et
-            reduceStack();
+            reduceStack(i - 1);
             reducedStack.push(tokenStack.pop().getTokenVal());
-            System.out.println("Stack : "+reducedStack.toString());
+            System.out.println("Stack : " + reducedStack.toString());
+            ++i;
         }
+        reduceStack(i-1);
         /*boolean res = canReduce("(/ EXPI EXPI)",EXPI_EXPRESSION);
         boolean res2 = canReduce("(or EXPB EXPB)",EXPB_EXPRESSION);
         System.out.println("Res1:"+res+" Res2:"+res2);*/
@@ -68,31 +74,54 @@ public class Parser {
         }*/
     }
 
-    private void reduceStack(){
+    private void reduceStack(int index) {
         // eger bos ise direk don ve elemanı eklet
-        if(reducedStack.empty())
-            return;
+        System.out.println("ReduceStack Index:" + index);
 
-        String pk = reducedStack.peek();
-        if(canReduce(pk,EXPI_EXPRESSION)){
-            String temp = "EXPI";
-            reducedStack.pop();
-            reducedStack.push(temp);
-            System.out.println("New Stack:"+reducedStack.toString());
+        try {
+
+            if (reducedStack.empty())
+                return;
+
+            String pk = reducedStack.peek();
+            System.out.println("Pk:"+pk);
+            if (tokenList.getAllTokens().get(index) instanceof Identifier) {
+                //System.out.println(tokenList.getAllTokens().get(index)+pk);
+                reducedStack.pop();
+                reducedStack.push("Id");
+            } else if (pk.equals(")")) {
+                // ( '( e kadar stackten cek, yoksa EXCEPTIOOOON
+                // exception fırlatabilir
+                StringBuilder sb=new StringBuilder();
+                while (!reducedStack.peek().equals("(") || reducedStack.peek().equals("'(")) {
+                    sb.insert(0,reducedStack.pop().toString());
+                }
+                System.out.println("Paranthesis SB:"+sb.toString());
+
+            }
+
+            if (canReduce(pk, EXPI_EXPRESSION)) {
+                String temp = "EXPI";
+                reducedStack.pop();
+                reducedStack.push(temp);
+                System.out.println("New Stack:" + reducedStack.toString());
+            }
+        } catch (EmptyStackException e) {
+            System.err.println("Stackte acma parantez bulunamadı kocumm");
         }
     }
 
 
-    private boolean isInput(String str){
-        if(str.equals("EXPI")| str.equals("EXPILISI"))
+    private boolean isInput(String str) {
+        if (str.equals("EXPI") | str.equals("EXPILISI"))
             return true;
         return false;
     }
 
-    private boolean canReduce(String str,String pattern){
-        boolean result=false;
-        Pattern r= Pattern.compile(pattern);
-        Matcher matcher=r.matcher(str);
+    private boolean canReduce(String str, String pattern) {
+        boolean result = false;
+        Pattern r = Pattern.compile(pattern);
+        Matcher matcher = r.matcher(str);
         return matcher.find();
     }
 }
