ce91a942e14ef5fdb5750a3d644928a3e650ca73
hmenn <hmen.56@gmail.com>
2016-12-26 20:22:29 +0300

    [project2] expi and expb completed

--- Project2/src/coffee/project/Parser.java
+++ Project2/src/coffee/project/Parser.java
@@ -4,6 +4,7 @@ import coffee.IdentifierList;
 import coffee.TokenList;
 import coffee.datatypes.*;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.EmptyStackException;
 import java.util.Stack;
@@ -45,63 +46,41 @@ public class Parser {
         tokenStack.addAll(tokenList.getAllTokens());
         Collections.reverse(tokenStack);
 
-        stackTOStr(tokenStack);
-
         System.out.println("Token List:" + tokenList.getAllTokens());
         System.out.println("Token Stack:" + tokenStack);
 
         StringBuilder sb1 = new StringBuilder();
+        output.push("");
 
         while (!tokenStack.empty()) {
+            System.out.println("------WHILE-----");
+            printStack(output);
             StringBuilder sb2 = new StringBuilder();
             // eger stack reduce edilebilir ise reduce et
-            sb2.append(sb1);
-            System.out.println("sb21:"+sb2.toString());
+            sb2.append(output.peek());
+            System.out.println("sb2:"+sb2.toString());
             Token token = tokenStack.pop();
-            sb2.append(token.getTokenVal());
-            System.out.println("sb22"+sb2.toString());
+            sb2.append(" ").append(token.getTokenVal());
+            System.out.println("sb2:"+sb2.toString());
             output.push(sb2.toString());
 
             if (token instanceof Identifier) {
                 reducedStack.push("Id");
-                sb1.append("Id");
-                output.push(sb1.toString());
+                output.push(" "+stackToStr(reducedStack));
             } else if (token instanceof ValueBinary) {
                 reducedStack.push("BinaryValue");
-                sb1.append("BinaryValue");
-                output.push(sb1.toString());
+                output.push(" "+stackToStr(reducedStack));
             } else if (token instanceof ValueInt) {
                 reducedStack.push("IntegerValue");
-                sb1.append("IntegerValue");
-                output.push(sb1.toString());
+                output.push(" "+stackToStr(reducedStack));
             } else {
                 reducedStack.push(token.getTokenVal());
             }
-            
             reduceStack();
-
         }
 
-        System.out.println("\nResStack:"+output.toString());
-        /*boolean res = canReduce("(/ EXPI EXPI)",EXPI_EXPRESSION);
-        boolean res2 = canReduce("(or EXPB EXPB)",EXPB_EXPRESSION);
-        System.out.println("Res1:"+res+" Res2:"+res2);*/
+        printStack(output);
 
-        /*for(Token t: tokenList.getAllTokens()){
-            String item=null;
-            if(t instanceof Operator)
-                item = ((Operator)t).getOperator();
-            else if (t instanceof Keyword)
-                item = ((Keyword)t).getKeyword();
-            else if (t instanceof Identifier)
-                item = ((Identifier)t).getName();
-            else if(t instanceof ValueBinary)
-                item = ((ValueBinary)t).getValue().toString();
-            else if(t instanceof ValueInt)
-                item=((ValueInt)t).getValue().toString();
-            stack.push(item);
-            //System.out.println("Parse Stack:"+stack.toString());
-        }*/
     }
 
     private void reduceStack() {
@@ -109,14 +88,15 @@ public class Parser {
 
         try {
             String peek = reducedStack.peek();
-            System.out.println("Peek:" + peek);
+            //System.out.println("Peek:" + peek);
             if (peek.equals("Id") || peek.equals("IntegerValue")) {
-                //System.out.println(tokenList.getAllTokens().get(index)+pk);
                 reducedStack.pop();
                 reducedStack.push("EXPI");
+                output.push(" "+stackToStr(reducedStack));
             } else if (peek.equals("BinaryValue")) {
                 reducedStack.pop();
                 reducedStack.push("EXPB");
+                output.push(" "+stackToStr(reducedStack));
             } else if (peek.equals(")")) {
                 // ( '( e kadar stackten cek, yoksa EXCEPTIOOOON
                 // exception fırlatabilir
@@ -126,19 +106,19 @@ public class Parser {
                     sb.insert(0," ");
                 }
                 sb.insert(0, reducedStack.pop().toString());
-                System.out.println("ReducedStackLast:"+reducedStack.toString());
+                //System.out.println("ReducedStackLast:"+reducedStack.toString());
                 System.out.println("Paranthesis SB:" + sb.toString());
 
                 if (canReduce(sb.toString(), EXPI_EXPRESSION)) {
                     reducedStack.push("EXPI");
-                    //System.out.println("New Stack:" + reducedStack.toString());
+                    output.push(" "+stackToStr(reducedStack));
                 }else if (canReduce(sb.toString(), EXPB_EXPRESSION)) {
                     reducedStack.push("EXPB");
-                    //System.out.println("New Stack:" + reducedStack.toString());
+                    System.out.println("hereee");
+                    output.push(" "+stackToStr(reducedStack));
                 }
 
             }
-
 
         } catch (EmptyStackException e) {
             System.err.println("Stackte acma parantez bulunamadı kocumm");
@@ -153,18 +133,23 @@ public class Parser {
     }
 
     private boolean canReduce(String str, String pattern) {
-        boolean result = false;
         Pattern r = Pattern.compile(pattern);
         Matcher matcher = r.matcher(str);
         return matcher.find();
     }
 
-    private String stackTOStr(Stack<Token> stack){
-
-        System.out.println(stack.peek());
-        for(Token token: stack){
-            System.out.println(token.getTokenVal());
+    private void printStack(Stack<String> stack){
+        System.out.println("Output:");
+        for(int i=stack.size()-1;i>=0;--i){
+            System.out.println(stack.get(i));
         }
-        return "";
+    }
+
+    private String stackToStr(Stack<String> stack){
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<stack.size();++i){
+            sb.append(stack.get(i)).append(" ");
+        }
+        return sb.toString();
     }
 }
